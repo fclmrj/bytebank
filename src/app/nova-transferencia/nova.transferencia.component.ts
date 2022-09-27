878b9c55e7b995444d635c8743f93fda39e632d7
y4bx <y4bx@petrobras.com.br>
2022-09-27 15:25:15 -0300

    03 Recebendo dados no componente

--- src/app/nova-transferencia/nova.transferencia.component.ts
+++ src/app/nova-transferencia/nova.transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector:'app-nova-transferencia',
@@ -8,12 +8,13 @@ import { Component } from "@angular/core";
 })
 export class NovaTransferenciaComponent{
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
   valor: number = 12;
   destino: number = 13;
 
   transferir(){
     console.log('Solicitada nova transferência');
-    console.log("Valor:", this.valor);
-    console.log("Destino:", this.destino);
+    this.aoTransferir.emit({valor: this.valor, destino: this.destino});
   }
 }
