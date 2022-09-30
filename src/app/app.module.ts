7881a88a361137e9e998d6b720b74fb40e629956
y4bx <y4bx@petrobras.com.br>
2022-09-30 15:57:04 -0300

    Angular explorando o framework Aula 5 - Atividade 2 Criando mensagem com ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NovaTransferenciaComponent } from './nova-transferencia/nova.transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { LOCALE_ID, DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova.transferencia.component';
 import { FormsModule } from '@angular/forms';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { registerLocaleData } from '@angular/common';
