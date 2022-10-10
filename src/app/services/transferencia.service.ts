0c3c4d62b75d68233cc54b0fd596a08ed072565c
y4bx <y4bx@petrobras.com.br>
2022-10-10 11:45:40 -0300

    Organizando o código

--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -11,27 +11,27 @@ export class TransferenciaService {
   listaTransferencias: any[];
   private url = 'http://localhost:3000/transferencias';
 
-constructor(private httpClient: HttpClient) {
-  this.listaTransferencias = [];
-}
+  constructor(private httpClient: HttpClient) {
+    this.listaTransferencias = [];
+  }
 
-get transferencias(){
-  return this.listaTransferencias;
-}
+  get transferencias(){
+    return this.listaTransferencias;
+  }
 
-get todas(): Observable<Transferencia[]>{
-  return this.httpClient.get<Transferencia[]>(this.url);
-}
+  get todas(): Observable<Transferencia[]>{
+    return this.httpClient.get<Transferencia[]>(this.url);
+  }
 
-adicionar(transferencia: any): Observable<Transferencia[]>{
-  console.log("Adicionando via service ..");
-  this.hidratar(transferencia);
+  adicionar(transferencia: any): Observable<Transferencia[]>{
+    console.log("Adicionando via service ..");
+    this.hidratar(transferencia);
 
-  return this.httpClient.post<Transferencia[]>(this.url, transferencia);
-}
+    return this.httpClient.post<Transferencia[]>(this.url, transferencia);
+  }
 
-private hidratar(transferencia: any){
-  transferencia.data = new Date();
-}
+  private hidratar(transferencia: any){
+    transferencia.data = new Date();
+  }
 
 }
